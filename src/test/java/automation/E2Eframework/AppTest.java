35b25cc513bc7282590cef0d15d13356fa35ae38
Gaurav Gupta <gauravgupta6891@gmail.com>
2022-07-24 09:54:50 +0530

    first commit

---
+++ src/test/java/automation/E2Eframework/AppTest.java
@@ -0,0 +1,20 @@
+package automation.E2Eframework;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
